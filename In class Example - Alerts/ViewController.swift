94872155f35b0d305942ebdf9644a9d169f5413b
Austin Gmuer <a.gmuer@setonhill.edu>
2020-02-18 11:32:55 -0500

    Initial Commit

---
+++ In class Example - Alerts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  In class Example - Alerts
+//
+//  Created by Austin Gmuer on 2/18/20.
+//  Copyright © 2020 Austin Gmuer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

40225fb14df14e8830e21fea6d8ef4478d7b0ba4
Austin Gmuer <a.gmuer@setonhill.edu>
2020-02-18 12:02:26 -0500

    Ready to Review

--- In class Example - Alerts/ViewController.swift
+++ In class Example - Alerts/ViewController.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+
+    @IBOutlet weak var usernameField: UITextField!
+    
+    
+    
+    //Alert Button
+    @IBAction func displayAlert(_ sender: Any) {
+        
+        // 1. Setup the Alert
+        let alert = UIAlertController (title: "Important Update!", message: "Software update available.", preferredStyle: .alert)
+        
+        // 2. Add the Action
+        alert.addAction(UIAlertAction(title: NSLocalizedString("Update", comment: "default action"), style: .`default`, handler: { _ in}))
+        
+        //Cancel Button
+         alert.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: "default action"), style: .`default`, handler: { _ in}))
+        
+        // 3. Display the Alert
+        self.present(alert,animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
